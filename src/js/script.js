80d374bf98cf5adba145990fab886201f9709c1f
GiannisTzonis <tzonis.john@hotmail.com>
2022-11-24 06:57:42 +0200

    moving cube with camera

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -15,9 +16,25 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+const orbit = new OrbitControls(camera, renderer.domElement);
+
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.z = 5;
+// camera.position.z = 5;
+// camera.position.y = 2;
+camera.position.set(0, 2, 5);
+orbit.update();
 
-renderer.render(scene, camera);
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+function animate(time) {
+  box.rotation.x = time / 1000;
+  box.rotation.y = time / 1000;
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
